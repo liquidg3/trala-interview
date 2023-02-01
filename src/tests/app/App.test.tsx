e37bea1f94a00e34304b15b2ea78c3830d912974
Taylor Romero <me@taylorrome.ro>
2023-01-31 17:26:30 -0700

    patch: refactor

--- src/tests/app/App.test.tsx
+++ src/tests/app/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../views/app/App';
+import App from '../../app/App';
 
 test('app renders login', async () => {
   render(<App />);
