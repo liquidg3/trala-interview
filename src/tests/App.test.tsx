95704c519918ee141b804ed7236fa02d4cbcce47
Taylor Romero <me@taylorrome.ro>
2023-01-31 17:25:22 -0700

    patch: failing test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../views/app/App';
 
-test('renders login', async () => {
+test('app renders login', async () => {
   render(<App />);
-
   await screen.findByTestId('login');
-
 });
--- src/views/app/App.tsx
+++ src/views/app/App.tsx
@@ -4,20 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div data-testid="login">
-          hey
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Coming soon
       </header>
     </div>
   );
