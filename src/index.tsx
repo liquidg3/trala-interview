36504d2340c6202e1bcba4b27cb8af11be9aabff
Taylor Romero <me@taylorrome.ro>
2023-01-31 17:22:24 -0700

    patch: sass support

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import './styles/index.sass';
 import App from './views/app/App';
 import reportWebVitals from './reportWebVitals';
 
